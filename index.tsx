f5334a223155b4bfb90fab77a3338cc566e39efe
Sanju1672 <105843878+Sanju1672@users.noreply.github.com>
2022-10-19 11:28:50 +0530

    2 files modified

--- index.tsx
+++ index.tsx
@@ -1,14 +1,15 @@
+import { TextField } from '@mui/material';
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import MyApps from './my-apps';
-import AppTile from './AppTile';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     <MyApps filterAppVersion="Version 1" />
   </StrictMode>
 );

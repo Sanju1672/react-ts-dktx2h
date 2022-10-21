c1875d262bcd0a75104409c170b66bbd69a2d4ef
Sanju1672 <105843878+Sanju1672@users.noreply.github.com>
2022-10-21 14:43:22 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import MyApps from './my-apps';
-import AuthenticationAppTile from './authentication/authentication-appTile';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <AuthenticationAppTile />
-    //
+    <MyApps />
+    
   </StrictMode>
 );
